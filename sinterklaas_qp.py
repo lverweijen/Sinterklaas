5912167e6cc7331075289ff421090ba95b0123de
lverweijen <lverweijen>
2018-10-22 00:37:20 +0200

    Improve formatting of output

--- sinterklaas_qp.py
+++ sinterklaas_qp.py
@@ -16,27 +16,29 @@ class Leader(object):
     name = attr.ib()
 
 
-participants = [
-    Child('lisa', 12, 1),
-    Child('piet', 9, -1),
-    [Child('jan', 8, -1), Child('dirk', 16, -1), Child('anne', 9, 1)],
-    [Child('bert', 6, -1), Child('ernie', 8, -1), Leader('dad of bert & ernie')],
+registrations = [
+    [Child('lisa', 12,'f')],
+    [Child('piet', 9, 'm')],
+    [Child('jan', 8, 'm'), Child('dirk', 12, 'm'), Child('anne', 9, 'f')],
+    [Child('bert', 6, 'm'), Child('ernie', 8, 'm'), Leader('dad of bert & ernie')],
 ]
 
 # Random children
 for k in range(20):
-    # gender = 2 * (k % 2) - 1
-    # gender = 2 * (k < 10) - 1
-    gender = 2 * (k & 7 < 3) - 1
-    participants.append(Child(str(k), 5 + (k % 7), gender))
+    if k % 6 < 3:
+        gender = 'm'
+    else:
+        gender = 'f'
+    child = Child(str(k), 5 + (k % 7), gender)
+    registrations.append([child])
 
 # Random leaders
-for k in range(5):
-    participants.append(Leader(str(k)))
+for k in range(4):
+    registrations.append([Leader(str(k))])
 
-print("Participants:")
-for participant in participants:
-    print(participant)
+print("Registrations:")
+for registration in registrations:
+    print(registration)
 
 print()
 
@@ -47,17 +49,14 @@ kid_relations = []
 leader_relations = []
 kid_leader_relations = []
 
-for participant in participants:
-    if not isinstance(participant, list):
-        participant = [participant]
-
+for registration in registrations:
     first_kid = None
     first_adult = None
 
-    for subparticipant in participant:
-        if isinstance(subparticipant, Child):
+    for participant in registration:
+        if isinstance(participant, Child):
             kid_index = len(kids)
-            kids.append(subparticipant)
+            kids.append(participant)
             if first_kid is not None:
                 kid_relations.append((first_kid, kid_index))
             else:
@@ -66,9 +65,9 @@ for participant in participants:
             if first_adult is not None:
                 kid_leader_relations.append((kid_index, first_adult))
 
-        elif isinstance(subparticipant, Leader):
+        elif isinstance(participant, Leader):
             adult_index = len(leaders)
-            leaders.append(subparticipant)
+            leaders.append(participant)
             if first_adult is not None:
                 leaders.append((first_adult, adult_index))
             else:
@@ -79,7 +78,7 @@ for participant in participants:
 
 
 
-groups = ['group ' + str(g) for g in range(3)]
+groups = ['group ' + str(g + 1) for g in range(3)]
 
 n = len(kids)
 m = len(groups)
@@ -116,12 +115,6 @@ for leader in leaders:
     print(leader)
 print()
 
-# print('Siblings:')
-# for pair in kid_relations:
-#     print("pair: {!r}".format(pair))
-#     print([kids[p] for p in pair], 'are siblings')
-# print()
-
 
 ages = [kid.age for kid in kids]
 means = group_means(ages, len(groups))
@@ -131,14 +124,9 @@ scores = np.array([np.abs(kid.age - means)**2 for kid in kids])
 x = cvx.Variable(shape=(n, m), name="kid_in_group", boolean=True)
 y = cvx.Variable(shape=(r, m), name="adult_in_group", boolean=True)
 
-genders = np.array([kid.sex for kid in kids])
+genders = np.array([(kid.sex == 'f') - (kid.sex == 'm') for kid in kids])
 
 age_opt = cvx.sum(cvx.multiply(scores, x))
-# sex_opt = 0
-# sex_opt = 1 * cvx.sum(cvx.abs(genders @ x))
-# sex_opt = 1 * cvx.sum(cvx.abs(genders @ x**2))
-# sex_opt = 1 * cvx.sum(cvx.multiply(genders, x), 0) ** 2
-# sex_opt = cvx.sum(cvx.sum(cvx.multiply(cvx.hstack(3 * [[genders]]), x), 0) ** 2)
 sex_opt = cvx.sum((genders * x) ** 2)
 leader_opt = 0.1 * cvx.sum(y)
 obj = cvx.Minimize(age_opt + sex_opt + leader_opt)
@@ -178,8 +166,11 @@ for (friend1, friend2) in kid_leader_relations:
 
 problem = cvx.Problem(obj, constraints)
 
-solution = problem.solve(verbose=False)
+print("problem.get_problem_data(): {!r}".format(problem.get_problem_data(cvx.ECOS_BB)))
+print()
 
+solution = problem.solve(verbose=False, solver=cvx.ECOS_BB)
+print("problem.status: {!r}".format(problem.status))
 print("solution: {!r}".format(solution))
 print("age_opt.value: {!r}".format(age_opt.value))
 print("sex_opt.value: {!r}".format(sex_opt.value))
@@ -192,18 +183,18 @@ print()
 from collections import defaultdict
 group_members = defaultdict(list)
 
-for k, kid in enumerate(kids):
-    g = np.argmax(x.value[k, :])
-    # print(kid, groups[g])
-    group_members[groups[g]].append(kid)
-
 for l, leader in enumerate(leaders):
     g = np.argmax(y.value[l, :])
     # print(leader, groups[g])
     group_members[groups[g]].append(leader)
 
+for k, kid in enumerate(kids):
+    g = np.argmax(x.value[k, :])
+    # print(kid, groups[g])
+    group_members[groups[g]].append(kid)
 
-for group, members in group_members.items():
+for group in sorted(group_members.keys()):
+    members = group_members[group]
     print(group)
     for member in members:
         print(member)
